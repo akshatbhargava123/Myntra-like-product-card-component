e7a09a128d54e2abb153e28d98f8219ce5e496a5
akshat <akshatbhargavadeepa@gmail.com>
2019-02-05 16:05:20 +0530

    added carousel in product card slider and made info dynamic

--- src/App.js
+++ src/App.js
@@ -6,10 +6,49 @@ class App extends Component {
   render() {
     return (
       <div style={{ background: "#FFFFFF", padding: "30px" }}>
+        {/* <ProductCard />
         <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
+        <ProductCard /> */}
+        <ProductCard
+          showCloseIcon
+          onCloseClick={() => console.log("closed!")}
+          product={{
+            name: "Men Navy Blue Solid Hooded Sweatshirt",
+            discountedPrice: 999,
+            actualPrice: 1999,
+            salePercent: 50,
+            pictures: [
+              "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/6997811/2018/11/15/a835f8e2-de81-4e9e-bc35-0a9d064737fd1542271007646-Indian-Terrain-Men-Navy-Blue-Solid-Hooded-Sweatshirt-9251542271007269-1.jpg",
+              "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/6997811/2018/11/15/377fc261-f719-4667-8856-9b3b527ca87c1542271007598-Indian-Terrain-Men-Navy-Blue-Solid-Hooded-Sweatshirt-9251542271007269-3.jpg",
+              "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/6997811/2018/11/15/a0bb6b2d-1e35-44b6-85fa-547bbfa794d31542271007579-Indian-Terrain-Men-Navy-Blue-Solid-Hooded-Sweatshirt-9251542271007269-4.jpg",
+              "https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/6997811/2018/11/15/b7f5b85b-fb7d-45fc-a8e9-03b31a9bc2e41542271007568-Indian-Terrain-Men-Navy-Blue-Solid-Hooded-Sweatshirt-9251542271007269-5.jpg"
+            ],
+            attributes: [
+              {
+                name: "Size",
+                values: [
+                  { name: "Small" },
+                  { name: "Medium" },
+                  { name: "Large" }
+                ]
+              },
+              {
+                name: "Quantity",
+                values: [
+                  { name: "1" },
+                  { name: "2" },
+                  { name: "3" },
+                  { name: "4" },
+                  { name: "5" },
+                  { name: "6" },
+                  { name: "7" },
+                  { name: "8" },
+                  { name: "9" }
+                ]
+              }
+            ]
+          }}
+        />
       </div>
     );
   }
--- src/ProductCard.js
+++ src/ProductCard.js
@@ -1,16 +1,14 @@
 import React from "react";
 import { Icon } from "antd";
+import { Carousel, Button } from "antd";
 import SelectOptions from "./SelectOptions";
 import "./ProductCard.css";
 
-const link =
-  "https://assets.myntassets.com/h_480,q_100,w_360/v1/assets/images/7713774/2018/11/27/2fb59bfa-0e42-443c-a201-58429f49e32f1543317173496-Clarks-Men-Tan-Sneakers-7531543317172788-1.jpg";
-
-const AttributeComponent = () => (
+const AttributeComponent = ({ attribute, onClose }) => (
   <>
     <div className="attribute-selector-header">
-      <div className="attribute-selector heading">Select a Size</div>
-      <div className="attribute-selector-close">
+      <div className="attribute-selector heading">Select {attribute.name}</div>
+      <div className="attribute-selector-close" onClick={onClose}>
         <Icon type="close" />
       </div>
     </div>
@@ -23,52 +21,158 @@ const AttributeComponent = () => (
       }}
     >
       <SelectOptions
-        defaultSelected={0}
-        options={["Large", "Medium", "Small"]}
+        options={attribute.values.map(v => v.name)}
+        onSelect={i => onClose(i)}
       />
     </div>
   </>
 );
 
-const ProductCard = () => {
-  const [isOpen, updateOpenState] = React.useState(false);
-  return (
-    <div className="main-card-container">
-      <div className="image-container">
-        <a>
-          <img className="product-image" alt="" src={link} />
-        </a>
-        <div className="remove-icon">
-          <Icon type="close" />
-        </div>
-        <div className={`card-shim ${isOpen && "card-shim-active"}`} />
-      </div>
+const slickCarouselSettings = {
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  swipeToSlide: true,
+  dots: false
+};
+
+class ProductCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false,
+      attributeValues: [],
+      attributesSelected: 0
+    };
+    this.Carousel = null;
+  }
+
+  startMoving = () => {
+    this.interval = setInterval(() => {
+      if (this.Carousel) this.Carousel.next();
+    }, 1300);
+  };
+
+  stopMoving = () => {
+    if (this.interval) {
+      clearInterval(this.interval);
+      this.interval = null;
+    }
+  };
+
+  onAttributeSelect = selected => {
+    if (
+      selected === undefined ||
+      selected == null ||
+      typeof selected != "number"
+    )
+      return this.setState({ isOpen: !this.state.isOpen });
+    if (this.state.attributesSelected >= this.props.product.attributes.length) {
+      if (this.props.onAddToCartClick)
+        this.props.onAddToCartClick(this.state.attributeValues);
+      return this.setState({
+        attributeValues: [],
+        attributesSelected: 0
+      });
+    }
+    this.setState(
+      {
+        attributeValues: [...this.state.attributeValues, selected],
+        attributesSelected: this.state.attributesSelected + 1 || 0
+      },
+      () => console.log(this.state)
+    );
+  };
+
+  render() {
+    const { product } = this.props;
+    const { isOpen, attributeValues } = this.state;
+
+    const {
+      name,
+      pictures,
+      attributes,
+      actualPrice,
+      discountedPrice,
+      salePercent
+    } = product;
+
+    console.log(product);
+
+    return (
       <div
-        className={`attribute-display-div ${isOpen &&
-          "show-attribute-display-div"}`}
+        className="main-card-container"
+        onMouseOver={this.startMoving}
+        onMouseOut={this.stopMoving}
       >
-        <AttributeComponent />
-      </div>
-      <div className="actions-container">
-        <div className="item-details">
-          <div className="item-title">Clarks Men Tan Brown Sneakers</div>
-          <div className="price-label-container">
-            <span className="item-details-strike">Rs.7,999</span>
-            <span className="item-details-bold">Rs.5,599</span>
-            <span className="item-details-discountPercent">(30% OFF)</span>
-          </div>
+        <div className="image-container">
+          <a>
+            <Carousel
+              ref={ref => {
+                this.Carousel = ref;
+              }}
+              {...slickCarouselSettings}
+            >
+              {pictures.map((picture, i) => {
+                return (
+                  <div>
+                    <img
+                      key={i}
+                      className="product-image"
+                      alt=""
+                      src={picture}
+                    />
+                  </div>
+                );
+              })}
+            </Carousel>
+          </a>
+          {this.props.showClose && (
+            <div className="remove-icon" onClick={this.props.onCloseClick}>
+              <Icon type="close" />
+            </div>
+          )}
+          <div className={`card-shim ${isOpen && "card-shim-active"}`} />
         </div>
         <div
-          className="item-action-div"
-          onClick={() => updateOpenState(!isOpen)}
+          className={`attribute-display-div ${isOpen &&
+            "show-attribute-display-div"}`}
         >
-          <a style={{ display: "block" }}>
-            {!isOpen ? "MOVE TO CART" : "DONE"}
-          </a>
+          {attributes[attributeValues.length] ? (
+            <AttributeComponent
+              attribute={attributes[attributeValues.length]}
+              onClose={this.onAttributeSelect}
+            />
+          ) : (
+            ""
+          )}
+        </div>
+        <div className="actions-container">
+          <div className="item-details">
+            <div className="item-title">{name}</div>
+            <div className="price-label-container">
+              <span className="item-details-bold">₹ {discountedPrice}</span>
+              <span className="item-details-strike">₹ {actualPrice}</span>
+              <span className="item-details-discountPercent">
+                ({salePercent}% OFF)
+              </span>
+            </div>
+          </div>
+          <div
+            className="item-action-div"
+            onClick={() => this.setState({ isOpen: !this.state.isOpen })}
+          >
+            <Button style={{ marginRight: 8 }} type="primary">
+              ADD TO CART
+            </Button>
+            <Button>WISHLIST</Button>
+            {/* <a style={{ display: "block" }}>{!isOpen ? "WISHLIST" : "DONE"}</a> */}
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default ProductCard;
